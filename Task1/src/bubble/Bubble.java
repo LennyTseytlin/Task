531de24f2cf7f26ecf7f3aad90332b613796f5db
LennyTseytlin <leonardus1996@yahoo.com>
2015-12-25 22:09:18 +0300

    Task1

--- Task1/src/bubble/Bubble.java
+++ Task1/src/bubble/Bubble.java
@@ -1,26 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bubble;
 import java.util.Scanner;
-/**
- *
- * @author Leonid
- */
-public class Bubble {
 
-    /**
-     * @param args the command line arguments
-     */
+public class Bubble {
      public static void main(String[] args) {
         double a[] = parse(args);
         bubble(a);
-        for (int i = 0; i < a.length; ++i)
+        for (int i = 0; i < a.length; i++)
             System.out.print(a[i]+" ");
         System.out.println();
     }
+     
     public static boolean bubble(double arr[]) {
       boolean swapped = true;
       int j = 0;
@@ -39,11 +28,11 @@ public class Bubble {
       }
      return swapped;
     }
-       private static double[] parse(String[] input) {
+    
+    private static double[] parse(String[] input) {
         double res[] = new double[input.length];
         for (int i = 0; i < input.length; i++)
             res[i] = Double.parseDouble(input[i]);
         return res;
-    }
-    
+    }    
 }
